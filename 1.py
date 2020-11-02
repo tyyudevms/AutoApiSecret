e059411300cfe6a8bba523a690c3da60db39b9a5
tyyudevms <73127782+tyyudevms@users.noreply.github.com>
2020-11-02 13:50:51 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -20,8 +20,8 @@ def gettoken(refresh_token, client_id, client_secret):
             }
     data={'grant_type': 'refresh_token',
           'refresh_token': refresh_token,
-          'client_id':id,
-          'client_secret':secret,
+          'client_id': client_id,
+          'client_secret': client_secret,
           'redirect_uri':'http://localhost:53682/'
          }
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
